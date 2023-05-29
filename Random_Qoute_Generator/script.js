482da596925510d340811f91d5039b0308a5d9b3
Kayode Oluwajuwon Olaoluwa <okayode986@stu.ui.edu.ng>
2023-05-29 01:56:51 +0100

    Add files via upload

---
+++ Random_Qoute_Generator/script.js
@@ -0,0 +1,115 @@
+/*
+  Code by Oluwajuwon Kayode for freeCodeCamp Front End Developer Certification Project
+*/
+
+const projectName = "random-quote-generator";
+let quotesData;
+
+let colors = [
+  "#16a085",
+  "#27ae60",
+  "#2c3e50",
+  "#f39c12",
+  "#e74c3c",
+  "#9b59b6",
+  "#FB6964",
+  "#342224",
+  "#472E32",
+  "#BDBB99",
+  "#77B1A9",
+  "#73A857",
+];
+let currentQuote, currentAuthor;
+currentQuote = currentAuthor = "";
+
+function getQuotes() {
+  //   return $.ajax({
+  //     headers: {
+  //       Accept: "application/json",
+  //     },
+  //     url: "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",
+  //     success: function (jsonQuotes) {
+  //       if (typeof jsonQuotes === "string") {
+  //         quotesData = JSON.parse(jsonQuotes);
+  //         console.log("quotesData");
+  //         console.log(quotesData);
+  //       }
+  //     },
+  //   });
+
+  let category = "success";
+  $.ajax({
+    method: "GET",
+    url: `https://api.api-ninjas.com/v1/quotes?category=${category}`,
+    headers: { "X-Api-Key": "xK2RlfM/3j8AfFL5lZYFNw==KsTwe7I7TiLpfWxA" },
+    contentType: "application/json",
+    success: function (result) {
+      quotesData = JSON.parse(result);
+      console.log(result);
+    },
+    error: function ajaxError(jqXHR) {
+      console.error("Error: ", jqXHR.responseText);
+    },
+  });
+}
+
+function getRandomQuote() {
+  return quotesData.quotes[
+    Math.floor(Math.random() * quotesData.quotes.length)
+  ];
+}
+
+function getQuote() {
+  let randomQuote = getRandomQuote();
+
+  currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
+
+  $("#tweet-quote").attr(
+    "href",
+    "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
+
+  $("#tumblr-quote").attr(
+    "href",
+    "https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=" +
+      encodeURIComponent(currentAuthor) +
+      "&content=" +
+      encodeURIComponent(currentQuote) +
+      "&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button"
+  );
+
+  $(".quote-text").animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $("#text").text(randomQuote.quote);
+  });
+
+  $(".quote-author").animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $("#author").html(randomQuote.author);
+  });
+
+  let color = Math.floor(Math.random() * colors.length);
+  $("html body").animate(
+    {
+      backgroundColor: colors[color],
+      color: colors[color],
+    },
+    1000
+  );
+  $(".button").animate(
+    {
+      backgroundColor: colors[color],
+    },
+    1000
+  );
+}
+
+$(document).ready(function () {
+  getQuotes().then(() => {
+    getQuote();
+  });
+
+  $("#new-quote").on("click", getQuote);
+});
